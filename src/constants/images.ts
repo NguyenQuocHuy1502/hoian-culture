29c60b166f8166ea17c59e1f8309d23049e10e2d
Huy <nguyenquochuy15022007@gmail.com>
2024-10-08 20:20:00 +0700

    first commit

---
+++ src/constants/images.ts
@@ -0,0 +1,51 @@
+import bgSplash from '@/assets/images/cultural/bg.jpeg'
+import logo from '@/assets/images/cultural/logo.png'
+import bg from '@/assets/images/cultural/bg.png'
+import beach from '@/assets/images/cultural/beach.png'
+import place from '@/assets/images/cultural/place.png'
+import craft from '@/assets/images/cultural/craft.png'
+import food from '@/assets/images/cultural/food.png'
+import hotel from '@/assets/images/cultural/hotel.png'
+import cafe from '@/assets/images/cultural/cafe.png'
+import hoian from '@/assets/images/cultural/hoian.png'
+import rungduabaymau from '@/assets/images/cultural/rungduabaymau.png'
+import banhmiphuong from '@/assets/images/cultural/banhmiphuong.png'
+import cafe1 from '@/assets/images/cultural/cafe1.png'
+import langnghehoian1 from '@/assets/images/cultural/langnghehoian1.jpg'
+
+import denlonghoian from '@/assets/images/cultural/langnghehoian1.jpg'
+import vuongiayviet from '@/assets/images/cultural/langnghehoian1.jpg'
+import tatoobamboo from '@/assets/images/cultural/langnghehoian1.jpg'
+import langcuilu from '@/assets/images/cultural/langnghehoian1.jpg'
+import timingmask from '@/assets/images/cultural/langnghehoian1.jpg'
+import cafephin from '@/assets/images/cultural/langnghehoian1.jpg'
+import vaivun from '@/assets/images/cultural/langnghehoian1.jpg'
+import muathiencau from '@/assets/images/cultural/langnghehoian1.jpg'
+import thuphap from '@/assets/images/cultural/langnghehoian1.jpg'
+
+export default {
+  logo,
+  bgSplash,
+  bg,
+  beach,
+  place,
+  food,
+  hotel,
+  cafe,
+  craft,
+  hoian,
+  rungduabaymau,
+  banhmiphuong,
+  cafe1,
+  langnghehoian1,
+  denlonghoian,
+  vuongiayviet,
+  tatoobamboo, 
+  langcuilu,
+  timingmask,
+  cafephin,
+  vaivun, 
+  muathiencau,
+  thuphap
+
+}
